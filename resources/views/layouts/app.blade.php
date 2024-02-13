18f05f70103318e1f7f668cdb631cb5880c18677
VelicuLiviu <velicu.liviu375@gmail.com>
2024-02-13 11:11:53 +0200

    Creare model si migrare

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Adaugă legături către fișierele CSS ale proiectului Laravel -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Adaugă legături către fișierele CSS ale SweetAlert 2 -->
+    <link href="{{ asset('sweet-alert/sweetalert2.min.css') }}" rel="stylesheet">
+</head>
+<body>
+    <!-- Adaugă legături către fișierele JavaScript ale proiectului Laravel -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Adaugă legături către fișierele JavaScript ale SweetAlert 2 -->
+    <script src="{{ asset('sweet-alert/sweetalert2.min.js') }}"></script>
+</body>
+</html>
