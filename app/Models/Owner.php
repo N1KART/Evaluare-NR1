241e0a44f97fd70a8b05ca5350f9b8fd7135aa02
N1KART <arteni_nicu@mail.ru>
2024-02-13 09:25:44 +0200

    create new brach

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Owner extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'name',
+        'car_id'
+    ];
+    public function user():BelongsTo {
+        return $this->belongsTo(Car::class);}
+    
 }
